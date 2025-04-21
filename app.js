90c34c8cdd57e340d52218b634b5dc27c4fa61d7
Milan Bastola <91013612+Milan342@users.noreply.github.com>
2025-04-21 15:40:14 +0545

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
-const port= process.env.PORT || 8080
 
 // Database connection
 main()
